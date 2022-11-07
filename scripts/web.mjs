65f94db3a959716730e2a7e3c0cfd163a0c0d087
Bruno <faithkidbruno@gmail.com>
2022-11-06 23:30:53 -0300

    changed all layout

--- scripts/web.mjs
+++ scripts/web.mjs
@@ -3,31 +3,11 @@ export default function webJS() {
     const queryAll = document.querySelectorAll.bind(document)
 
     //* FRAMES
-    const alertFrame = query('#alertFrame')
-    const homeFrame = query('#homeFrame')
-    const projectsFrame = query('#projectsFrame')
-    const bioFrame = query('#bioFrame')
     const miniAlert = query('#miniAlert')
     const bioBox = query('#bioBox')
     const contactBox = query('#contactBox')
     const referenceBox = query('#referenceBox')
     const conteudoBox = query('#conteudoBox').children
-
-    //* SHOWING ALERT FRAME
-    alertFrame.classList.add('moved-right')
-
-    //* BUTTONS
-
-    //? alert frame
-    const alertBTN1 = query('#alertBTN1').addEventListener('click', alertBTNs)
-    const alertBTN2 = query('#alertBTN2').addEventListener('click', alertBTNs)
-
-    //? home frame
-    const projectsBTN = query('#projetos').addEventListener('click', () => {
-        hideOpt()
-        frameAway(homeFrame)
-        frameCall(projectsFrame)
-    })
 
     //? projects frame
 
@@ -48,11 +28,6 @@ export default function webJS() {
     })
 
 
-    const nextBTN = query('#nextBTN').addEventListener('click', () => {
-        frameAway(projectsFrame)
-        frameCall(bioFrame)
-    })
-
     //? bio frame
     const contactMeBTN = query('#contactMeBTN').addEventListener('click', () => {
         cleanConteudoBox()
@@ -68,12 +43,6 @@ export default function webJS() {
         referenceBox.classList.add('show')
     })
 
-    const backHomeBTN = query('#backHomeBTN').addEventListener('click', () => {
-        showOpt()
-        frameAway(bioFrame)
-        frameCall(homeFrame)
-    })
-
     //? Mini alert animate
 
     Array.from(queryAll('.option')).forEach(opt => {
@@ -87,36 +56,6 @@ export default function webJS() {
     })
 
     //* FUNCTIONS
-
-    function alertBTNs() {
-        showOpt()
-        frameAway(alertFrame)
-        frameCall(homeFrame)
-    }
-
-    function showOpt() {
-        queryAll('.option').forEach(opt => {
-            opt.classList.add('show')
-        })
-    }
-
-    function hideOpt() {
-        queryAll('.option').forEach(opt => {
-            opt.classList.remove('show')
-        })
-    }
-
-    function frameAway(el) {
-        el.classList.add('moved-left')
-        el.classList.remove('moved-right')
-        setTimeout(() => {
-            el.classList.remove('moved-left')
-        }, 3500);
-    }
-
-    function frameCall(el) {
-        el.classList.add('moved-right')
-    }
 
     function cleanConteudoBox() {
         Array.from(conteudoBox).map(child => { child.classList.remove('show') })
